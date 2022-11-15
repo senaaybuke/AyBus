70296847db35f7c0499e63766cde0c5c37169bc2
Sena Aybüke <s.aybukeaslan@gmail.com>
2022-11-15 14:06:45 +0300

    V1

---
+++ src/main/java/com/aybuke/AyBus/repository/entity/Seat.java
@@ -0,0 +1,14 @@
+package com.aybuke.AyBus.repository.entity;
+
+import lombok.Builder;
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Table
+@Entity
+@Data
+@Builder
+public class Seat {
+}
